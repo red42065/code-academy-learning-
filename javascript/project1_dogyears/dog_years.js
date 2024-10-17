1e15ad4012f63d48cf76946b9075dabcda34d4d0
Sucheth Mekala <93038593+red42065@users.noreply.github.com>
2024-10-17 12:40:46 -0500

    Create dog_years.js

---
+++ javascript/project1_dogyears/dog_years.js
@@ -0,0 +1,10 @@
+let myAge = 23;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *=4;
+console.log(laterYears,earlyYears);
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = "sucheth".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} 
+years old in human years which is ${myAgeInDogYears} years old in dog years.`);

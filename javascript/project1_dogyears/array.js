71c6e9fc56bdda5cecfa3e01488e89cd6795bc24
Sucheth Mekala <93038593+red42065@users.noreply.github.com>
2024-10-28 14:13:13 -0500

    Create array.js

---
+++ javascript/project1_dogyears/array.js
@@ -0,0 +1,50 @@
+let secretMessage = [
+  "Learning",
+  "is",
+  "not",
+  "about",
+  "what",
+  "you",
+  "get",
+  "easily",
+  "the",
+  "first",
+  "time,",
+  "it",
+  "is",
+  "about",
+  "what",
+  "you",
+  "can",
+  "figure",
+  "out.",
+  "-2015,",
+  "Chris",
+  "Pine,",
+  "Learn",
+  "JavaScript",
+];
+console.log(secretMessage.length);
+secretMessage.pop();
+console.log(secretMessage);
+console.log(secretMessage.length);
+
+console.log(secretMessage.length);
+secretMessage.push("to");
+secretMessage.push("Program");
+console.log(secretMessage);
+console.log(secretMessage.length);
+
+secretMessage[7] = "right";
+console.log(secretMessage);
+
+secretMessage.shift();
+console.log(secretMessage);
+
+secretMessage.unshift("Programming");
+console.log(secretMessage);
+
+secretMessage.splice(7, 4, "know,");
+console.log(secretMessage);
+
+console.log(secretMessage.join());
